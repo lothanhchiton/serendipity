a9c2fd1d94550725076eeab0b93919ec9955c894
lothanhchiton <4nmnnax@gmail.com>
2026-06-20 14:21:08 +0800

    motion blur

--- shaders/lib/core/settings.glsl
+++ shaders/lib/core/settings.glsl
@@ -145,3 +145,9 @@ vec3 handPosition2 = vec3(-0.9, -0.65, -near - 0.1);
 #define DOF_BOKEH_SHAPE 0 // [0 5 6 8]
 
 #define DOF_BLUR_SKY
+
+#define MOTION_BLUR
+#define MOTION_BLUR_STRENGTH 1.0 // [0.7 0.8 0.9 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0 5.2 5.4 5.6 5.8 6.0 6.2 6.4 6.6 6.8 7.0]
+#define MOTION_BLUR_MAX_PX 32.0 // [16.0 32.0 64.0]
+#define MOTION_BLUR_SAMPLES 16 // [8 16 24 32 40 48 52 60 68 72]
+#define MOTION_BLUR_SKY
